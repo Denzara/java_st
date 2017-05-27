afc74ca1d6a53ce57edf3981721ae48cc24362c7
Roza Korochkina <denzara@gmail.com>
2017-05-27 23:57:15 +0300

    Рефакторинг

--- sandbox/src/main/java/ru/stqa/st/sandbox/FirstJavaprog.java
+++ sandbox/src/main/java/ru/stqa/st/sandbox/FirstJavaprog.java
@@ -1,7 +1,7 @@
 package ru.stqa.st.sandbox;
 
-public class FirstJavaprog{
-	public static void main (String[] args){
-System.out.println("Hello,world");
-	}
+public class FirstJavaprog {
+  public static void main(String[] args) {
+    System.out.println("Hello,world");
+  }
 }

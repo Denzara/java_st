e03704200cdf2af4a2ac106400931e25a1188f10
Roza Korochkina <denzara@gmail.com>
2017-05-28 14:32:01 +0300

    Добавлены классы

---
+++ sandbox/src/main/java/ru/stqa/st/sandbox/Rectangle.java
@@ -0,0 +1,14 @@
+package ru.stqa.st.sandbox;
+
+/**
+ * Created by Nick on 28.05.2017.
+ */
+public class Rectangle {
+  public double a;
+  public double b;
+
+  public Rectangle(double a,double b){
+    this.a = a;
+    this.b = b;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/st/sandbox/Square.java
@@ -0,0 +1,12 @@
+package ru.stqa.st.sandbox;
+
+/**
+ * Created by Nick on 28.05.2017.
+ */
+public class Square {
+  public double l;
+
+  public  Square(double l){
+    this.l=l;
+  }
+}
